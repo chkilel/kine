3c52c28ce638ad6a0212fbdc01df5e692d2adc0c
Adil Chehabi <adil@chehabi.ma>
2025-10-16 14:35:49 +0100

    fix(database): set default and update functions for updatedAt column

--- server/database/schema/columns.helpers.ts
+++ server/database/schema/columns.helpers.ts
@@ -4,7 +4,10 @@ export const timestamps = {
   createdAt: integer({ mode: 'timestamp_ms' })
     .$default(() => new Date())
     .notNull(),
-  updatedAt: integer({ mode: 'timestamp_ms' }).notNull()
+  updatedAt: integer({ mode: 'timestamp_ms' })
+    .notNull()
+    .$default(() => new Date())
+    .$onUpdateFn(() => new Date())
 }
 
 export const timestampsSoftDelete = {
